23b2c6c6c2066156d8a977e6c8b8daa9828af37d
Gordon Guthrie <gordon@vixo.com>
2013-08-17 21:52:40 +0000

    more changes

--- test/supported/types.erl
+++ test/supported/types.erl
@@ -7,8 +7,9 @@ types() ->
     B  = 2.3,
     C1 = true,
     C2 = blue,
+    C3 = 'Blue',
     D  = [a, b],
     E  = {1, 2},
     F  = "string",
     G = make_ref(),
-    {A, B, C1, C2, D, E, F, G}.
+    {A, B, C1, C2, C3, D, E, F, G}.
