328b9f703cf2e8542e342d23fea4a837d8e0e680
Gordon Guthrie <gordon@vixo.com>
2013-08-25 10:14:51 +0000

    more tests

--- test/not_passing/src/basic_types2.erl
+++ test/not_passing/src/basic_types2.erl
@@ -13,3 +13,12 @@ tuple() ->
 ref() ->
     C = make_ref(),
     C.
+
+chars() ->
+    [$n, $o, $w, $, $\t, $i, $s, $\t, $t, $h, $e, $\t, $w, $i, $n, $t, $e, $r].
+
+radishes() ->
+    [2#10, 16#104, 32#4545].
+
+exponents() ->
+    [2.3e+9, 2.3e-9].
