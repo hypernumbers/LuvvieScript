315ac6ef616bc7d8d5cf3912f7ca76791d3b91c0
Gordon Guthrie <gordon@vixo.com>
2013-10-11 19:15:06 +0000

    new test module for how multi-arity fns are to be handled

---
+++ test/not_passing/src/basic_functions.erl
@@ -0,0 +1,33 @@
+-module(basic_functions).
+
+%%% this module is designed to test the rejigging of functions that have one clause
+
+-export([
+         zero_arity/0
+        ]).
+
+zero_arity() ->
+    A = arity(a),
+    B = arity("B", 2),
+    C = arity(3, 5, "C33"),
+    {A, B, C}.
+
+arity(_One, _Two, _Three) ->
+    berk.
+
+arity(erko) ->
+    limbo;
+arity("one") ->
+    rhubarbo;
+arity(One) when is_list(One) ->
+    interpolate(One);
+arity(_One) ->
+    erko.
+
+interpolate(Yando) ->
+    smando.
+
+arity(one, _Two) ->
+    shambo;
+arity("One", _Two) ->
+    bambo.
